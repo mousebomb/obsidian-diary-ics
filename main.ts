52acd699514a899fea08dab97232d4ebc7ca5a0b
mousebomb <mousebomb@gmail.com>
2025-05-15 11:27:55 +0800

    更改图标

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import {addIcon, App, Notice, Plugin, PluginSettingTab, Setting, TFile} from 'obsidian';
 import * as http from 'http';
 import { createEvents, EventAttributes } from 'ics';
 import { networkInterfaces } from 'os';
@@ -59,8 +59,9 @@ export default class DiaryIcsPlugin extends Plugin {
 		const localIP = this.getLocalIP();
 
 		// 添加图标到左侧边栏
+		addIcon("diary-ics",`<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-sync-icon lucide-calendar-sync"><path d="M11 10v4h4"/><path d="m11 14 1.535-1.605a5 5 0 0 1 8 1.5"/><path d="M16 2v4"/><path d="m21 18-1.535 1.605a5 5 0 0 1-8-1.5"/><path d="M21 22v-4h-4"/><path d="M21 8.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h4.3"/><path d="M3 10h4"/><path d="M8 2v4"/></svg>`);
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const ribbonIconEl = this.addRibbonIcon('calendar-with-checkmark', 'Diary ICS', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('diary-ics', 'Diary ICS', (evt: MouseEvent) => {
 			// 点击图标时显示ICS订阅链接
 			new Notice(`ICS订阅链接: http://${localIP}:${this.settings.port}/feed.ics`);
 		});
@@ -464,7 +465,7 @@ class DiaryIcsSettingTab extends PluginSettingTab {
 
 		new Setting(containerEl)
 			.setName('日记文件夹')
-			.setDesc('日记文件所在的文件夹路径，留空表示根目���')
+			.setDesc('日记文件所在的文件夹路径，留空表示根目录')
 			.addText(text => text
 				.setPlaceholder('')
 				.setValue(this.plugin.settings.diaryFolder)
