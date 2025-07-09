9dea57cb392a50170fd6b4a47045e3e916db466b
mousebomb <mousebomb@gmail.com>
2025-07-09 12:39:46 +0800

    更换为内置的icon

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {addIcon, App, getLanguage, Notice, Plugin, PluginSettingTab, Setting, TFile, moment} from 'obsidian';
+import {App, getLanguage, Notice, Plugin, PluginSettingTab, Setting, TFile, moment} from 'obsidian';
 import * as http from 'http';
 import { createEvents, EventAttributes } from 'ics';
 import { networkInterfaces } from 'os';
@@ -64,9 +64,9 @@ export default class DiaryIcsPlugin extends Plugin {
 		const localIP = this.getLocalIP();
 
 		// 添加图标到左侧边栏
-		addIcon("diary-ics",`<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-sync-icon lucide-calendar-sync"><path d="M11 10v4h4"/><path d="m11 14 1.535-1.605a5 5 0 0 1 8 1.5"/><path d="M16 2v4"/><path d="m21 18-1.535 1.605a5 5 0 0 1-8-1.5"/><path d="M21 22v-4h-4"/><path d="M21 8.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h4.3"/><path d="M3 10h4"/><path d="M8 2v4"/></svg>`);
+		// addIcon("diary-ics",`<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-sync-icon lucide-calendar-sync"><path d="M11 10v4h4"/><path d="m11 14 1.535-1.605a5 5 0 0 1 8 1.5"/><path d="M16 2v4"/><path d="m21 18-1.535 1.605a5 5 0 0 1-8-1.5"/><path d="M21 22v-4h-4"/><path d="M21 8.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h4.3"/><path d="M3 10h4"/><path d="M8 2v4"/></svg>`);
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const ribbonIconEl = this.addRibbonIcon('diary-ics', this.locale.pluginName, (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('calendar-heart', this.locale.pluginName, (evt: MouseEvent) => {
 			// 点击图标时显示ICS订阅链接
 			new Notice(`${this.locale.pluginName}: http://${localIP}:${this.settings.port}/feed.ics`);
 		});
